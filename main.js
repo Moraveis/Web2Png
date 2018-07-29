b0bf7e482733618f955a4bf239dbc84f520e540c
Moraveis <joao@codebit.com.br>
2018-07-28 23:11:30 -0300

    implementation of webp convertion

--- main.js
+++ main.js
@@ -1,20 +1,41 @@
 var fs = require('fs');
- 
-if (process.argv.length <= 2) {
+var webp = require('webp-converter');
+
+if (process.argv.length <= 3) {
     console.log("Usage: " + __filename + " path/to/directory");
     process.exit(-1);
 }
  
-var path = process.argv[2];
- 
-fs.readdir(path, function(err, items) {
-    for (var i=0; i<items.length; i++) {
-        var file = path + '/' + items[i];
- 
-        console.log("Start: " + file);
-        fs.stat(file, generate_callback(file));
+var from = process.argv[2];
+var to = process.argv[3];
+
+main(from, to);
+
+function main(from, to){
+    if(!fs.existsSync(to)){
+        fs.mkdirSync(to);
     }
-});
+
+    fs.readdir(from, function(err, items) {
+        if(err != undefined)
+            console.log(err);
+        else
+            for (var i=0; i<items.length; i++) {
+                var file = from + '/' + items[i];
+                var fileConv = to + '/' + items[i].replace(".webp", ".png");
+        
+                console.log("Start: " + file);
+                fs.stat(file, generate_callback(file));
+
+                webp.dwebp(file, fileConv, "-o", function(status){
+                    if(status === '100')
+                        console.log("Imagem '" + file + "' exicuted successfully");
+                    else
+                        console.log("exicuted unsuccessfully status " + status);
+                });
+            }
+    });
+}
  
 function generate_callback(file) {
     return function(err, stats) {
