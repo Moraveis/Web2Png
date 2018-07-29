e0b31054a48157f9a359190da611eaf458e02999
Moraveis <joao@codebit.com.br>
2018-07-28 22:41:33 -0300

    implementation for reading files from directory

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 var fs = require('fs');
-
-if(process.argv.length <= 2){
-    console.log("Usage: " + __filename + "path/to");
+ 
+if (process.argv.length <= 2) {
+    console.log("Usage: " + __filename + " path/to/directory");
     process.exit(-1);
 }
-
-var path = process.argv[2];
-
-fs.stat(path, function(err, stats){
-    console.log(path);
-    console.log();
-    console.log(stats);
-    console.log();
-
-    if (stats.isFile()) {
-        console.log('    file');
-    }
-    if (stats.isDirectory()) {
-        console.log('    directory');
-    }
  
-    console.log('    size: ' + stats["size"]);
+var path = process.argv[2];
+ 
+fs.readdir(path, function(err, items) {
+    for (var i=0; i<items.length; i++) {
+        var file = path + '/' + items[i];
+ 
+        console.log("Start: " + file);
+        fs.stat(file, generate_callback(file));
+    }
 });
+ 
+function generate_callback(file) {
+    return function(err, stats) {
+            console.log(file);
+            console.log(stats["size"]);
+        }
+};
