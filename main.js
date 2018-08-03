a3bb08c53a9e58829142676c9c76b8cc154dcc47
Moraveis <joao@codebit.com.br>
2018-08-02 23:09:02 -0300

    implementing loop

--- main.js
+++ main.js
@@ -1,45 +1,69 @@
 var fs = require('fs');
 var webp = require('webp-converter');
 
-if (process.argv.length <= 3) {
+if (process.argv.length <= 2) {
     console.log("Usage: " + __filename + " path/to/directory");
     process.exit(-1);
 }
  
-var from = process.argv[2];
-var to = process.argv[3];
+var path = process.argv[2];
 
-main(from, to);
-
-function main(from, to){
-    if(!fs.existsSync(to)){
-        fs.mkdirSync(to);
-    }
-
-    fs.readdir(from, function(err, items) {
+function read_folder(path){
+    fs.readdir(path, function(err, items){
         if(err != undefined)
             console.log(err);
-        else
-            for (var i=0; i<items.length; i++) {
-                var file = from + '/' + items[i];
-                var fileConv = to + '/' + items[i].replace(".webp", ".png");
-        
-                console.log("Start: " + file);
-                fs.stat(file, generate_callback(file));
+        else{
+            var stop_loop = false;
+            for(var i = 0; i < items.length; i++){
+                if(stop_loop)
+                    break;
+                else {
+                    var new_path = path.endsWith("/") ? path.concat(items[1]) : path.concat("/").concat(items[1]);
+                    fs.stat(new_path, function(err, stats){
+                        if(err != undefined)
+                            console.log(err);
+                        else{
+                            if(stats.isDirectory())
+                                read_folder(new_path);
+                            else
+                                stop_loop = true;
+                        }
+                    });
+                }
+            }
+            convert(path);
+        }
+    });
+}
 
-                webp.dwebp(file, fileConv, "-o", function(status){
+function convert(folder){
+    var destiny = folder.concat("_conv");
+
+    if(!fs.existsSync(destiny)){
+        fs.mkdirSync(destiny);
+    }
+
+    fs.readdir(folder, function(err, items) {
+        if(err != undefined)
+            console.log(err);
+        else { 
+            for (var i=0; i<items.length; i++) {
+                var original = folder + '/' + items[i];
+                var conv = destiny + "/" + items[i].replace(".webp", ".png");
+
+                webp.dwebp(original, conv, "-o", function(status){
                     if(status === '100')
-                        console.log("Imagem '" + file + "' exicuted successfully");
+                        console.log("Imagem '" + conv + "' exicuted successfully");
                     else
                         console.log("exicuted unsuccessfully status " + status);
                 });
             }
+        }
     });
 }
- 
-function generate_callback(file) {
-    return function(err, stats) {
-            console.log(file);
-            console.log(stats["size"]);
-        }
-};
+
+function main(path){
+    read_folder(path);
+}
+
+main(path);
